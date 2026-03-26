140275283c2162274ec43f59b3bdf26d78a69873
김도윤 <kdy0096@gmail.com>
2026-03-27 00:39:04 +0900

    Initial Commit

---
+++ FavoriteNews/FavoriteNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteNewsApp.swift
+//  FavoriteNews
+//
+//  Created by yoonie on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
